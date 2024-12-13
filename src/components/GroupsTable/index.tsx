bfb18026f67a66fb0e11100ec777087900f746ab
Pedro Henrique PHD <danesvara2002@gmail.com>
2024-12-13 12:21:35 -0300

    feat: add GroupsTable

---
+++ src/components/GroupsTable/index.tsx
@@ -0,0 +1,97 @@
+import { Card, Container, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography } from "@mui/material";
+import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
+import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
+import { useTheme } from "@mui/styles";
+import { useNavigate } from "react-router";
+import { GroupDetail } from "src/models/Group";
+import { useAuth } from "src/utils/auth";
+import { userRequests } from "src/utils/requests";
+
+type Props = {
+  groupsList: GroupDetail[];
+  refreshList: () => void;
+};
+
+const GroupsTable = ({ groupsList, refreshList }: Props) => {
+  const { handlePermissionExists } = useAuth();
+
+  const { deleteGroup } = userRequests();
+
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+  const handleEditGroup = (id: number) => {
+    navigate(`/groups/edit/${id}`);
+  }
+
+  const handleDeleteGroup = async (id: number) => {
+    await deleteGroup(id);
+    refreshList();
+  }
+
+  return (
+    <Container maxWidth='lg'>
+      <Card>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Nome</TableCell>
+                <TableCell align="right">Ações</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {groupsList.map((group) => (
+                <TableRow hover key={group.id}>
+                  <TableCell>
+                    <Typography
+                      fontWeight="bold"
+                      gutterBottom
+                    >
+                      #{group.id}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      fontWeight="bold"
+                      gutterBottom
+                    >
+                      {group.name}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography
+                      fontWeight="bold"
+                      gutterBottom
+                    >
+                      {
+                      handlePermissionExists('change_group') && 
+                        <Tooltip title="Editar cargo" arrow>
+                          <IconButton>
+                            <EditTwoToneIcon onClick={() => handleEditGroup(group.id)}/>
+                          </IconButton>
+                        </Tooltip>
+                      }
+
+                      {
+                      handlePermissionExists('delete_group') &&
+                        <Tooltip title="Excluir cargo" arrow>
+                          <IconButton>
+                            <DeleteTwoToneIcon onClick={() => handleDeleteGroup(group.id)}/>
+                          </IconButton>
+                        </Tooltip>
+                      }
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Card>
+    </Container>
+  );
+};
+
+export default GroupsTable;
